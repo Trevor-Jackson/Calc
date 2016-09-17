33addf6d0f598c0e76ce52f399d2ed041c202fff
Trevor-Jackson <trevorj@exigo.com>
2016-09-16 22:24:24 -0500

    Calculator

---
+++ WebForm1.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Calculator.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            text-align: center;
+        }
+        #form1 {
+            text-align: center;
+        }
+        .auto-style2 {
+            font-size: large;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="auto-style1">
+    
+        <strong><span class="auto-style2">Simple Calculator<br />
+        </span></strong>
+        <br />
+&nbsp; First Value&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Second Value<br />
+        <asp:TextBox ID="FirstBox" runat="server" Width="99px"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="secondBox" runat="server" Width="99px"></asp:TextBox>
+    
+    </div>
+        <div class="auto-style1">
+&nbsp;<br />
+&nbsp;<asp:Button ID="plusButton" runat="server" OnClick="plusButton_Click" Text="+" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="minusButton" runat="server" OnClick="minusButton_Click" Text="-" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="divideButton" runat="server" OnClick="divideButton_Click" Text="/" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="multiplyButton" runat="server" OnClick="multiplyButton_Click" Text="*" />
+            <br />
+            <br />
+            <asp:Button ID="ResetButton" runat="server" OnClick="ResetButton_Click" Text="Reset" />
+            <br />
+            <br />
+&nbsp;&nbsp;
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="Error" runat="server" style="color: #FF0000"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="exceptionType" runat="server" style="color: #FF0000"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void plusButton_Click(object sender, EventArgs e)
+        {
+            try { 
+                double num1 = double.Parse(FirstBox.Text);
+                double num2 = double.Parse(secondBox.Text);
+                double result;
+                result = num1 + num2;
+                resultLabel.Text = result.ToString();
+                Error.Text = string.Empty;
+                exceptionType.Text = string.Empty;
+            } catch (Exception exception) {
+                exceptionType.Text = exception.Message;
+                Error.Text = "Please insert a number // fill in all fields"; }
+        }
+
+        protected void minusButton_Click(object sender, EventArgs e)
+        {
+            try {
+                double num1 = double.Parse(FirstBox.Text);
+                double num2 = double.Parse(secondBox.Text);
+                double result;
+                result = num1 - num2;
+                resultLabel.Text = result.ToString();
+                Error.Text = string.Empty;
+                exceptionType.Text = string.Empty;
+            } catch (Exception exception) {
+                exceptionType.Text = exception.Message;
+                Error.Text = "Please insert a number // fill in all fields";}
+        }
+
+        protected void divideButton_Click(object sender, EventArgs e)
+        {
+            try {
+                double num1 = double.Parse(FirstBox.Text);
+                double num2 = double.Parse(secondBox.Text);
+                double result;
+                result = num1 / num2;
+                resultLabel.Text = result.ToString();
+                Error.Text = string.Empty;
+                exceptionType.Text = string.Empty;
+            } catch (Exception exception) {             
+                exceptionType.Text = exception.Message;
+                Error.Text = "Please insert a number // fill in all fields"; }
+        }
+
+        protected void multiplyButton_Click(object sender, EventArgs e)
+        {
+            try {
+                double num1 = double.Parse(FirstBox.Text);
+                double num2 = double.Parse(secondBox.Text);
+                double result;
+                result = num1 * num2;
+                resultLabel.Text = result.ToString();
+                exceptionType.Text = string.Empty;
+                Error.Text = string.Empty;
+            } catch (Exception exception) {
+                exceptionType.Text = exception.Message;
+                Error.Text = "Please insert a number // fill in all fields"; }
+        }
+
+        protected void ResetButton_Click(object sender, EventArgs e)
+        {
+            FirstBox.Text = string.Empty;
+            secondBox.Text = string.Empty;
+            resultLabel.Text = string.Empty;
+            exceptionType.Text = string.Empty;
+            Error.Text = string.Empty;
+        }
+    }
+}
